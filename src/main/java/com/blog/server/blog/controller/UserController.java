a4077ca190cefa7e9cb547385a958dccc1bfe4db
JSOI <jsoiworks@gmail.com>
2022-06-13 22:40:24 +0900

    Add restiction to register new user & User controller, service, dto refactoring

--- src/main/java/com/blog/server/blog/controller/UserController.java
+++ src/main/java/com/blog/server/blog/controller/UserController.java
@@ -5,6 +5,7 @@ import com.blog.server.blog.dto.Response;
 import com.blog.server.blog.dto.UserDto;
 import com.blog.server.blog.repository.UserRepository;
 import com.blog.server.blog.security.JwtTokenProvider;
+import com.blog.server.blog.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -17,37 +18,22 @@ import java.util.Collections;
 @RequestMapping("/api")
 public class UserController {
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final JwtTokenProvider jwtTokenProvider;
+    private final UserService userService;
 
     //회원등록
     @PostMapping("/register")
-    public Response.Simple register(@RequestBody UserDto.Register userRegister, @AuthenticationPrincipal User user) {
-        User newUser = User.builder().name(userRegister.getName())
-                .nickname(userRegister.getNickname())
-                .email(userRegister.getEmail())
-                .password(passwordEncoder.encode(userRegister.getPassword()))
-                .introduce(userRegister.getIntroduce())
-                .roles(Collections.singletonList("ROLE_USER"))
-                .build();
-        userRepository.save(newUser);
-        return Response.Simple.builder().result(true).build();
+    public Response.Simple register(@RequestBody UserDto.Register userRegister) {
+        return userService.register(userRegister);
     }
 
     //로그인
     @PostMapping("/login")
-    public Response.Login login(@RequestBody UserDto.Login loginDto, @AuthenticationPrincipal User user) {
-        User targetUser = userRepository.findByEmail(loginDto.getEmail()).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다"));
-        if (!passwordEncoder.matches(loginDto.getPassword(), targetUser.getPassword())) {
-            throw new IllegalArgumentException("잘못된 비밀번호입니다.");
-        }
-        String token = jwtTokenProvider.createToken(targetUser.getEmail(), targetUser.getId(), targetUser.getRoles());
-        return Response.Login.builder().result(true).token(token).nickname(targetUser.getNickname()).build();
+    public Response.Login login(@RequestBody UserDto.Login loginDto) {
+        return userService.login(loginDto);
     }
 
     @GetMapping("/user")
     public User userInfo(@AuthenticationPrincipal User user) {
         return userRepository.findById(user.getId()).orElseThrow(() -> new IllegalArgumentException("UserID가 존재하지 않습니다."));
     }
-    //회원정보조회 - Token으로 치환 필요
 }
--- src/main/java/com/blog/server/blog/domain/User.java
+++ src/main/java/com/blog/server/blog/domain/User.java
@@ -2,6 +2,7 @@ package com.blog.server.blog.domain;
 
 
 import com.blog.server.blog.dto.UserDto;
+import com.blog.server.blog.validaton.Validator;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.AllArgsConstructor;
