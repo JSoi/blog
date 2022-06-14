f1304822be3988b958e88c8bee866731ffd28530
JSOI <jsoiworks@gmail.com>
2022-06-14 20:15:24 +0900

    ResponseDTO 생성

--- src/main/java/com/blog/server/blog/controller/UserController.java
+++ src/main/java/com/blog/server/blog/controller/UserController.java
@@ -6,9 +6,13 @@ import com.blog.server.blog.dto.UserDto;
 import com.blog.server.blog.excpetion.BlogException;
 import com.blog.server.blog.repository.UserRepository;
 import com.blog.server.blog.service.UserService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 import static com.blog.server.blog.excpetion.ErrorCode.USER_NOT_EXIST;
 
@@ -32,7 +36,31 @@ public class UserController {
     }
 
     @GetMapping("/user")
-    public User userInfo(@AuthenticationPrincipal User user) {
-        return userRepository.findById(user.getId()).orElseThrow(() -> new BlogException(USER_NOT_EXIST));
+    public UserInfo userInfo(@AuthenticationPrincipal User user) {
+        User findUser = userRepository.findById(user.getId()).orElseThrow(() -> new BlogException(USER_NOT_EXIST));
+        return new UserInfo(findUser);
+    }
+
+    //ResponseUser 필요
+    @Data
+    @AllArgsConstructor
+    static class UserInfo {
+        private String name;
+        private String nickname;
+        private String email;
+        private String introduce;
+        private String profile_image_url;
+        private LocalDateTime createdAt;
+
+
+        public UserInfo(User user) {
+            this.name = user.getName();
+            this.nickname = user.getNickname();
+            this.email = user.getEmail();
+            this.profile_image_url = user.getProfile_image_url();
+            this.introduce = user.getIntroduce();
+            this.createdAt = user.getCreatedAt();
+
+        }
     }
 }
