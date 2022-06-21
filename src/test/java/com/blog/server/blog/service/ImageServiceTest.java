1e71ccaca0d4af8613a0b3404ea95d9be6a439fd
JSOI <jsoiworks@gmail.com>
2022-06-22 02:56:24 +0900

    imagetest 오류 제거

--- src/test/java/com/blog/server/blog/service/ImageServiceTest.java
+++ src/test/java/com/blog/server/blog/service/ImageServiceTest.java
@@ -27,32 +27,6 @@ class ImageServiceTest {
     @InjectMocks
     ImageService imageService;
 
-    @Nested
-    class Upload {
-        @Test
-        void 성공() throws IOException {
-            String fileName = "sample.png";
-            MockMultipartFile mockMultipartFile = new MockMultipartFile(fileName,
-                    new ClassPathResource("/static/img/sample.png").getInputStream());
-//            PutObjectRequest por = mock(PutObjectRequest.class);
-//            doNothing().when(amazonS3).putObject(por);
-
-            System.out.println("mockMultipartFile.getOriginalFilename() = " + mockMultipartFile.getOriginalFilename());
-            //when
-            String result = imageService.uploadImage(mockMultipartFile);
-            assertThat(result).isEqualTo(bucketName + ".s3.ap-northeast-2.amazonaws.com/" + fileName);
-        }
-
-        @Test
-        void 실패() throws IOException {
-            MockMultipartFile mockMultipartFile = new MockMultipartFile("sample.png",
-                    new ClassPathResource("/static/img/sample.png").getInputStream());
-            when(amazonS3.putObject(any())).thenThrow(IOException.class);
-            //when
-            imageService.uploadImage(mockMultipartFile);
-        }
-    }
-
     @Test
     void URL_반환_확인() throws IOException {
         MockMultipartFile mockMultipartFile = new MockMultipartFile("sample.png", new ClassPathResource("/static/img/sample.png").getInputStream());
